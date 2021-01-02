37c12371163412d9e9f4ceaec9bfbc00569e1814
Freddie Duffield <fwhduffield@gmail.com>
2021-01-02 19:20:42 +0000

    bestTimeToBuy

--- bestTimeToBuy.js
+++ bestTimeToBuy.js
@@ -1,18 +1,14 @@
 /*
-1. find the earliest lowest index
-2. find the highest price after the index 
+Say you have an array prices for which the ith element is the price of a given stock on day i.
 
-1. create another sorted the array 
-2. find the lowest number with the earliest index in the orig array
-3. difference between that and the highest number in range.
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
 
-1a. an array of array of all profits
-1b. an array of profits
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
 */
 
 function bruteForce(prices) {
   return calculate(prices, 0);
-};
+}
 
 function calculate(prices, s) {
   if (s >= prices.length) return 0;
@@ -24,7 +20,7 @@ function calculate(prices, s) {
     for (let i = start + 1; i < prices.length; i++) {
       if (prices[start] < prices[i]) {
         let profit = calculate(prices, i + 1) + prices[i] - prices[start];
-        
+
         if (profit > maxProfit) {
           maxProfit = profit;
         }
@@ -39,7 +35,46 @@ function calculate(prices, s) {
 }
 
 function peakValley(prices) {
+  let i = 0;
+  // initiate peak valleys with first price
+  let valley = prices[0];
+  let peak = prices[0];
 
+  let maxProfit = 0;
+  // while not at last price
+  while (i < prices.length - 1) {
+    // find valley
+    while (i < prices.length - 1 && prices[i] >= prices[i + 1]) {
+      i++;
+    }
+    valley = prices[i];
+
+    while (i < prices.length - 1 && prices[i] <= prices[i + 1]) {
+      i++;
+    }
+
+    peak = prices[i];
+    maxProfit += peak - valley;
+  }
+  return maxProfit;
 }
 
-module.exports = {bruteForce, peakValley};
+// function simpleOnePass(prices) {
+//   let maxProfit = 0;
+//   for(let i = 1; i < prices.length; i++) {
+//     if(prices[i] > prices[i-1]) {
+//       maxProfit += prices[i] - prices[i-1];
+//     }
+//   }
+//   return maxProfit;
+// }
+
+const simpleOnePass = (prices) =>
+  prices.reduce((acc, curr, i) => {
+    if (prices[i] > prices[i - 1]) {
+      acc += prices[i] - prices[i - 1];
+    }
+    return acc;
+  }, 0);
+
+module.exports = { bruteForce, peakValley, simpleOnePass };
--- bestTimeToBuy.test.js
+++ bestTimeToBuy.test.js
@@ -1,7 +1,8 @@
-const {bruteForce, peakValley} = require('./bestTimeToBuy');
+const {bruteForce, peakValley, simpleOnePass} = require('./bestTimeToBuy');
+
 describe('bestTimeToBuy', () => {
  
-  describe.only('bruteForce', () => {
+  describe('bruteForce', () => {
     describe('bruteForce', () => {
       it('should return 7 from [7,1,5,3,6,4]', () => {
         expect(bruteForce([7,1,5,3,6,4])).toEqual(7);
@@ -10,9 +11,9 @@ describe('bestTimeToBuy', () => {
       it('should return 8 from [3,1,3,4,5,9]', () => {
         expect(bruteForce([3,1,3,4,5,9])).toEqual(8);
       });
-      
-      it('should return 8 from [3,1,3,4,5,9]', () => {
-        expect(bruteForce([3,1,3,4,5,9])).toEqual(8);
+
+      it('should return 4 from [1,2,3,4,5]', () => {
+        expect(bruteForce([1,2,3,4,5])).toEqual(4);
       });
     });
     
@@ -21,12 +22,18 @@ describe('bestTimeToBuy', () => {
         expect(peakValley([7,1,5,3,6,4])).toEqual(7);
       });
       
-      it('should return 8 from [3,1,3,4,5,9]', () => {
-        expect(peakValley([3,1,3,4,5,9])).toEqual(8);
+      it('should return 4 from [1,2,3,4,5]', () => {
+        expect(peakValley([1,2,3,4,5])).toEqual(4);
+      });
+    });
+
+    describe('simpleOnePass', () => {
+      it('should return 7 from [7,1,5,3,6,4]', () => {
+        expect(simpleOnePass([7,1,5,3,6,4])).toEqual(7);
       });
       
-      it('should return 8 from [3,1,3,4,5,9]', () => {
-        expect(peakValley([3,1,3,4,5,9])).toEqual(8);
+      it('should return 4 from [1,2,3,4,5]', () => {
+        expect(simpleOnePass([1,2,3,4,5])).toEqual(4);
       });
     });
   });
